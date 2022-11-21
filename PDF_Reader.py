d185d563efddfa961563d57ad376934eb1b7eff2
CaioKolarovic <caiocjkc@gmail.com>
2022-11-20 21:18:22 -0300

    Commit criando tabela de todas as páginas

--- PDF_Reader.py
+++ PDF_Reader.py
@@ -3,34 +3,64 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 
-#Ler o pdf e extrair apenas a primeira página
+
+#Ler o pdf e criar as variáveis
 reader = pyf.PdfFileReader("Example.pdf")
-page = reader.pages[0]
-page = page.extractText()
+complete_table = []
+i = 0
+qtd_negociacao = 0
 
-#Substituir o espaçamento e separar a string
-page = page.replace('\n', ' ;**; ')
-page = page.split(' ;**; ')
+#Enquanto existir páginas
+while True:
+    try:
+        page = reader.pages[i]
+        page = page.extractText()
 
-#Contar quantas linhas serão necessárias e até onde finalizar a lista
-qtd_negociacao = page.count('1-BOVESPA')
-final_lista = ((qtd_negociacao+1)*9)-1
+        #Substituir o espaçamento e separar a string
+        page = page.replace('\n', ' ;**; ')
+        page = page.split(' ;**; ')
+        
+        
+        #Contar quantas linhas serão necessárias e até onde finalizar a lista
+        qtd_negociacao_pagina = page.count('1-BOVESPA')
+        final_lista = ((qtd_negociacao_pagina+1)*9)
+        
+        #Limpar a lista com os dados necessários
+        page = page[2:5]+page[6:]
 
-#Limpar a lista com os dados necessários
-page = page[2:5]+page[6:]
-page = page[:final_lista]
+        #Acrescentar a 'coluna' extra em casos específicos
+        for word in page:
+            if 'FII' in word:
+                page.insert(((page.index(word))+1),'NM')
+            if 'EJ ' in word:
+                page.remove('EJ ')
 
-#Separar o cabeçalho dos dados
-header = page[:9]
-table = page[9:]
+        page = page[:final_lista]
+        
 
-#Acrescentar a 'coluna' extra em caso de FII
-for word in table:
-    if 'FII' in word:
-        table.insert(((table.index(word))+1),'NM')
+        #Separar o cabeçalho dos dados
+        header = page[:9]
+        table = page[9:]
+        
+        #Lista com as negociações de todas as páginas
+        complete_table.append(table)
+        i += 1
+
+        #Soma de quantas linhas teremos na DF
+        qtd_negociacao = qtd_negociacao + qtd_negociacao_pagina
+
+    #Acabaram as páginas    
+    except IndexError:
+        break
+
+    
+
+print(complete_table)
+flat_list = [item for sublist in complete_table for item in sublist]
 
 #Modificar a lista para criar o DF
-table = np.array(table)
+table = np.array(flat_list)
+print(table)
 reshaped = table.reshape(qtd_negociacao,9)
 df = pd.DataFrame(data = reshaped, columns = header)
 print(df)
