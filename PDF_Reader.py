986577c30b54813c4e72f1dc4117b7de2102da51
CaioKolarovic <caiocjkc@gmail.com>
2022-11-20 22:34:22 -0300

    Terceiro Commit acrescentando data ao DataFrame

--- PDF_Reader.py
+++ PDF_Reader.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 
-
 #Ler o pdf e criar as variáveis
 reader = pyf.PdfFileReader("Example.pdf")
 complete_table = []
@@ -15,11 +14,16 @@ while True:
     try:
         page = reader.pages[i]
         page = page.extractText()
-
+        x = 0
+        s = 9
         #Substituir o espaçamento e separar a string
         page = page.replace('\n', ' ;**; ')
         page = page.split(' ;**; ')
-        
+        for word in page:
+            if 'Data pregão' in word:
+                index = page.index(word) + 1
+                date = page[index]
+                
         
         #Contar quantas linhas serão necessárias e até onde finalizar a lista
         qtd_negociacao_pagina = page.count('1-BOVESPA')
@@ -37,11 +41,20 @@ while True:
 
         page = page[:final_lista]
         
-
         #Separar o cabeçalho dos dados
         header = page[:9]
+        header.insert(10, 'Data')
         table = page[9:]
-        
+
+        #Acrescentar a data               
+        while x <= qtd_negociacao_pagina:
+            table.insert(s, date)
+            s += 10
+            x += 1
+
+        #Não sei por que acrescentando uma data extra
+        table.pop()
+                
         #Lista com as negociações de todas as páginas
         complete_table.append(table)
         i += 1
@@ -55,13 +68,12 @@ while True:
 
     
 
-print(complete_table)
+#Transforma a lista de listas em uma lista única
 flat_list = [item for sublist in complete_table for item in sublist]
 
 #Modificar a lista para criar o DF
 table = np.array(flat_list)
-print(table)
-reshaped = table.reshape(qtd_negociacao,9)
+reshaped = table.reshape(qtd_negociacao,10)
 df = pd.DataFrame(data = reshaped, columns = header)
 print(df)
 df.to_csv('ExampleControl.csv')
